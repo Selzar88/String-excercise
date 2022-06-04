64ae3d1001fa2e9ff0537b8542f2fa030ca20cba
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:15:59 +0100

    Add project files.

---
+++ String excercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace String_excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter a character to search: ");
+            char searchInput = Console.ReadLine()[0];
+            int searchIndex = input.IndexOf(searchInput);
+            Console.WriteLine("index of character {0} in string is {1}", searchInput, searchIndex);
+
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName  = Console.ReadLine();
+
+            string fullName = string.Concat(firstName, " ", lastName);
+            Console.WriteLine(" Your full name is {0}", fullName);
+
+            Console.ReadKey();
+        }
+    }
+}
